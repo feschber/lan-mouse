1c3cd775a390e63cba321154d9f9ba8926d89a60
Ferdinand Schober <ferdinand.schober@prodato.de>
2023-02-13 17:40:29 +0100

    add todo

--- src/backend/windows/producer.rs
+++ src/backend/windows/producer.rs
@@ -4,5 +4,5 @@ use std::sync::mpsc::SyncSender;
 use crate::{event::Event, client::{ClientHandle, Client}, request::Server};
 
 pub fn run(_produce_tx: SyncSender<(Event, ClientHandle)>, _server: Server, _clients: Vec<Client>) {
-
+    todo!();
 }
