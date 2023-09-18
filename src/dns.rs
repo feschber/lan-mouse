96708a011e7ea2ce6cf995cccd1c87257224d7fb
Ferdinand Schober <ferdinand.schober@fau.de>
2023-09-18 15:56:54 +0200

    add dns resolve logging

--- src/dns.rs
+++ src/dns.rs
@@ -3,6 +3,7 @@ use std::{error::Error, net::IpAddr};
 use trust_dns_resolver::Resolver;
 
 pub fn resolve(host: &str) -> Result<Vec<IpAddr>, Box<dyn Error>> {
+    log::info!("resolving {host} ...");
     let response = Resolver::from_system_conf()?.lookup_ip(host)?;
     Ok(response.iter().collect())
 }
