a86d74b52ce31953bbeee72967e1bf23e89bdf8c
Ferdinand Schober <ferdinand.schober@fau.de>
2024-03-21 12:45:47 +0100

    update to reis 0.2

--- nix/default.nix
+++ nix/default.nix
@@ -27,10 +27,6 @@ rustPlatform.buildRustPackage {
 
   cargoLock.lockFile = ../Cargo.lock;
 
-  cargoLock.outputHashes = {
-    "reis-0.1.0" = "sha256-QhsRIkzW3wgOlcHpkx3axjS8Vfed00Uf36av9ossPwQ=";
-  };
-
   # Set Environment Variables
   RUST_BACKTRACE = "full";
 
