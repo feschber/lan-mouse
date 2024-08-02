b2ba49f955d2705963256ab07c3e6a21485bd136
Ferdinand Schober <ferdinand.schober@fau.de>
2024-08-02 18:02:32 +0200

    asdf

--- src/server/network_sec.rs
+++ src/server/network_sec.rs
@@ -39,31 +39,58 @@ pub(crate) async fn new(
     };
     let host = SocketAddr::new("0.0.0.0".parse().unwrap(), server.port.get());
     let listener = Arc::new(listen(host, cfg).await?);
-    Ok(tokio::task::spawn_local(async move {
-        loop {
-            tokio::select! {
-                result = listener.accept() => {
-                    match result {
-                        Ok((dtls_conn, addr))  => {
-                            let udp_recv_tx = udp_recv_tx.clone();
-                            tokio::task::spawn_local(async move {
-                                loop {
-                                    let mut buf = vec![0u8; 0];
-                                    while let Ok(_) = dtls_conn.recv(&mut buf).await {
-                                        let event = buf.as_slice().try_into()?;
-                                        let addr = dtls_conn.remote_addr()?;
-                                        let event = Ok((event, addr));
-                                        udp_recv_tx.send(event).await;
-                                    }
+    Ok(tokio::task::spawn_local())
+}
+
+async fn network_task(server: Server) {
+    loop {
+        tokio::select! {
+            result = listener.accept() => {
+                match result {
+                    Ok((dtls_conn, addr))  => {
+                        let udp_recv_tx = udp_recv_tx.clone();
+                        tokio::task::spawn_local(async move {
+                            loop {
+                                let mut buf = vec![0u8; 0];
+                                while let Ok(_) = dtls_conn.recv(&mut buf).await {
+                                    let event = buf.as_slice().try_into()?;
+                                    let addr = dtls_conn.remote_addr()?;
+                                    let event = Ok((event, addr));
+                                    udp_recv_tx.send(event).await;
                                 }
-                                Ok::<(),NetworkError>(())
-                            });
-                        },
-                        Err(e) => log::warn!("connecting failed {e}"),
-                    }
+                            }
+                            Ok::<(),NetworkError>(())
+                        });
+                    },
+                    Err(e) => log::warn!("connecting failed {e}"),
                 }
-                _ = server.cancelled() => break,
             }
+            _ = server.cancelled() => break,
         }
-    }))
+    }
+}
+
+/// load_key Load/read key from file
+pub fn load_key(path: PathBuf) -> Result<CryptoPrivateKey, Error> {
+    let f = File::open(path)?;
+    let mut reader = BufReader::new(f);
+    let mut buf = vec![];
+    reader.read_to_end(&mut buf)?;
+
+    let s = String::from_utf8(buf).expect("utf8 of file");
+
+    let key_pair = KeyPair::from_pem(s.as_str()).expect("key pair in file");
+
+    Ok(CryptoPrivateKey::from_key_pair(&key_pair).expect("crypto key pair"))
+}
+
+/// load_certificate Load/read certificate(s) from file
+pub fn load_certificate(path: PathBuf) -> Result<Vec<CertificateDer<'static>>, Error> {
+    let f = File::open(path)?;
+
+    let mut reader = BufReader::new(f);
+    match rustls_pemfile::certs(&mut reader).collect::<Result<Vec<_>, _>>() {
+        Ok(certs) => Ok(certs.into_iter().map(CertificateDer::from).collect()),
+        Err(_) => Err(Error::ErrNoCertificateFound),
+    }
 }
