1841bd081c0afbfc85209053f878f4b4c7627b8b
awnlzw <awnlzw@126.com>
2024-10-06 18:33:57 +0800

    nix: add desktop and icon

--- nix/default.nix
+++ nix/default.nix
@@ -44,6 +44,9 @@ rustPlatform.buildRustPackage {
   postInstall = ''
     wrapProgram "$out/bin/lan-mouse" \
       --set GDK_PIXBUF_MODULE_FILE ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+
+    install -Dm444 *.desktop -t $out/share/applications
+    install -Dm444 lan-mouse-gtk/resources/*.svg -t $out/share/icons/hicolor/scalable/apps
   '';
 
   meta = with lib; {
