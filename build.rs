3e1c3e95b73a26554154b0bf7387912e258ac74a
Ferdinand Schober <ferdinand.schober@fau.de>
2025-01-27 13:15:02 +0100

    use shadow-rs instead of executing git describe
    this removes git from the build dependencies

--- build.rs
+++ build.rs
@@ -1,20 +1,8 @@
-use std::process::Command;
+use shadow_rs::ShadowBuilder;
 
 fn main() {
-    // commit hash
-    let git_describe = Command::new("git")
-        .arg("describe")
-        .arg("--always")
-        .arg("--dirty")
-        .arg("--tags")
-        .output()
-        .map(|output| String::from_utf8(output.stdout).ok())
-        .ok()
-        .flatten()
-        .unwrap_or_else(|| {
-            println!("cargo:warning=Failed to get git describe");
-            String::from("unknown")
-        });
-    let git_describe = git_describe.trim().to_string();
-    println!("cargo::rustc-env=GIT_DESCRIBE={git_describe}");
+    ShadowBuilder::builder()
+        .deny_const(Default::default())
+        .build()
+        .expect("shadow build");
 }
