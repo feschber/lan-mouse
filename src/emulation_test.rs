2490a27b6c4bfad86d33fa95524555bba2f7f6b3
Raidon Chrome <soniczerops@gmail.com>
2025-10-31 20:45:19 +0400

    implemented mouse config

--- src/emulation_test.rs
+++ src/emulation_test.rs
@@ -1,6 +1,6 @@
 use crate::config::Config;
 use clap::Args;
-use input_emulation::{InputEmulation, InputEmulationError};
+use input_emulation::{InputConfig, InputEmulation, InputEmulationError};
 use input_event::{Event, PointerEvent};
 use std::f64::consts::PI;
 use std::time::{Duration, Instant};
@@ -22,7 +22,8 @@ pub async fn run(config: Config, _args: TestEmulationArgs) -> Result<(), InputEm
     log::info!("running input emulation test");
 
     let backend = config.emulation_backend().map(|b| b.into());
-    let mut emulation = InputEmulation::new(backend).await?;
+    let input_config: InputConfig = config.input_config();
+    let mut emulation = InputEmulation::new(backend, input_config).await?;
     emulation.create(0).await;
 
     let start = Instant::now();
