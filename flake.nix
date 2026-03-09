9900ec18ea44188d4cfebf742dd08448f68ef73b
onelocked <onelock@mail.com>
2026-03-09 21:46:00 +0000

    nix: minor flake improvements/maintenance

--- flake.nix
+++ flake.nix
@@ -2,65 +2,94 @@
   description = "Nix Flake for lan-mouse";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-    genSystems = lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-
-        overlays = [
-          rust-overlay.overlays.default
-        ];
-      };
-    mkRustToolchain = pkgs:
-      pkgs.rust-bin.stable.latest.default.override {
-        extensions = ["rust-src"];
-      };
-    pkgs = genSystems (system: import nixpkgs {inherit system;});
-  in {
-    packages = genSystems (system: rec {
-      default = pkgs.${system}.callPackage ./nix {};
-      lan-mouse = default;
-    });
-    homeManagerModules.default = import ./nix/hm-module.nix self;
-    devShells = genSystems (system: let
-      pkgs = pkgsFor system;
-      rust = mkRustToolchain pkgs;
-    in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          rust
-          rust-analyzer-unwrapped
-          pkg-config
-          xorg.libX11
-          gtk4
-          libadwaita
-          librsvg
-          xorg.libXtst
-        ] ++ lib.optionals stdenv.isDarwin
-        (with darwin.apple_sdk_11_0.frameworks; [
-          CoreGraphics
-          ApplicationServices
-        ]);
-
-        RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
-      };
-    });
-  };
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      rust-overlay,
+      self,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forEachPkgs =
+        f:
+        lib.genAttrs (import systems) (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
+            };
+            # Default toolchain for devshell
+            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+              extensions = [
+                # includes already:
+                # rustc
+                # cargo
+                # rust-std
+                # rust-docs
+                # rustfmt-preview
+                # clippy-preview
+                "rust-analyzer"
+                "rust-src"
+              ];
+            };
+            # Minimal toolchain for builds (rustc + cargo + rust-std only)
+            rustToolchainForBuild = pkgs.rust-bin.stable.latest.minimal;
+          in
+          f { inherit pkgs rustToolchain rustToolchainForBuild; }
+        );
+    in
+    {
+      packages = forEachPkgs (
+        { pkgs, rustToolchainForBuild, ... }:
+        let
+          customRustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchainForBuild;
+            rustc = rustToolchainForBuild;
+          };
+          lan-mouse = pkgs.callPackage ./nix { rustPlatform = customRustPlatform; };
+        in
+        {
+          default = lan-mouse;
+          inherit lan-mouse;
+        }
+      );
+      devShells = forEachPkgs (
+        { pkgs, rustToolchain, ... }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                rustToolchain
+                pkg-config
+                gtk4
+                libadwaita
+                librsvg
+              ]
+              ++ lib.optionals pkgs.stdenv.isLinux [
+                libX11
+                libXtst
+              ]
+              ++ lib.optionals pkgs.stdenv.isDarwin (
+                with darwin.apple_sdk_11_0.frameworks;
+                [
+                  CoreGraphics
+                  ApplicationServices
+                ]
+              );
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+      homeManagerModules.default = import ./nix/hm-module.nix self;
+    };
 }
